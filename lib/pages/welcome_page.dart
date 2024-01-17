aefeb65d8525d87a408c53f96ae5cda76e9f508e
Paulmuia <paulmuia7511@gmail.com>
2024-01-17 14:45:50 +0300

    added images

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -2,23 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:four/utils/page_view.dart';
 
 class WelcomePage extends StatelessWidget {
-  
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-   
     return const Scaffold(
-      backgroundColor: Colors.amber,
-      body: Column(
-        children: [
-          SizedBox(
-            height: 80,
-          ),
-        PageViewUtil(),
-        PageViewUtil()
-
-        ],
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            PageViewUtil(
+              imagePath: 'images/choma.webp',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            PageViewUtil(
+              imagePath: 'images/pilau.webp',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            PageViewUtil(
+              imagePath: 'images/pizza.webp',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            PageViewUtil(
+              imagePath: 'images/chapati.webp',
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/utils/page_view.dart
+++ lib/utils/page_view.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
 
 class PageViewUtil extends StatelessWidget {
-  const PageViewUtil({super.key});
+  final String imagePath;
+  const PageViewUtil({super.key, required this.imagePath});
 
   @override
   Widget build(BuildContext context) {
     PageController pageController = PageController(viewportFraction: 0.8);
     return SizedBox(
-            height: 200,
-            child: PageView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 10,
-                controller: pageController,
-                itemBuilder: (context, index) {
-                  return Container(
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                  //  padding: EdgeInsets.symmetric(horizontal: 100),
-                    height: 300,
-                    width: double.maxFinite,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      shape: BoxShape.rectangle,
-                      color: Colors.orange,
-                    ),
-                  );
-                }),
-          );
+      height: 200,
+      child: PageView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          controller: pageController,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              height: 300,
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  shape: BoxShape.rectangle,
+                  color: Colors.orange,
+                  image: DecorationImage(
+                      image: AssetImage(imagePath), fit: BoxFit.cover)),
+            );
+          }),
+    );
   }
 }
